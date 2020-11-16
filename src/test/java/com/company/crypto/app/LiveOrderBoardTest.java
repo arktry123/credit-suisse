c04fea105f9fad0fcca738e869729afc94a8240e
Ramakrishna <arktry123@gmail.com>
2020-11-16 02:14:08 +0000

    adding more test cases

--- src/test/java/com/company/crypto/app/LiveOrderBoardTest.java
+++ src/test/java/com/company/crypto/app/LiveOrderBoardTest.java
@@ -82,4 +82,109 @@ public class LiveOrderBoardTest {
 
         Assertions.assertThat(orderBoard.viewLiveOrders(OrderType.BUY)).isEqualTo(Collections.EMPTY_MAP);
     }
+
+    @Test
+    void testBuyForMixedOrders() {
+        orderBoard.addOrder(new Order(OrderType.BUY, "userid1", "cointype", 10, 700));
+        orderBoard.addOrder(new Order(OrderType.BUY, "userid2", "cointype", 11, 600));
+        orderBoard.addOrder(new Order(OrderType.BUY, "userid3", "cointype", 10, 500));
+        orderBoard.addOrder(new Order(OrderType.SELL, "userid4", "cointype", 10, 400));
+
+        HashMap<Integer, Integer> expectedOutput = new LinkedHashMap<>();
+        expectedOutput.put(700, 10);
+        expectedOutput.put(600, 11);
+        expectedOutput.put(500, 10);
+
+        Assertions.assertThat(orderBoard.viewLiveOrders(OrderType.BUY)).isEqualTo(expectedOutput);
+    }
+
+    @Test
+    void testSellForMixedOrders() {
+        orderBoard.addOrder(new Order(OrderType.SELL, "userid1", "cointype", 10, 700));
+        orderBoard.addOrder(new Order(OrderType.BUY, "userid2", "cointype", 11, 600));
+        orderBoard.addOrder(new Order(OrderType.SELL, "userid3", "cointype", 10, 500));
+        orderBoard.addOrder(new Order(OrderType.SELL, "userid4", "cointype", 10, 400));
+
+        HashMap<Integer, Integer> expectedOutput = new LinkedHashMap<>();
+        expectedOutput.put(400, 10);
+        expectedOutput.put(500, 10);
+        expectedOutput.put(700, 10);
+
+        Assertions.assertThat(orderBoard.viewLiveOrders(OrderType.SELL)).isEqualTo(expectedOutput);
+    }
+
+    @Test
+    void testSellWhenSamePriceForDifferentOrders() {
+        orderBoard.addOrder(new Order(OrderType.SELL, "userid1", "cointype", 10, 400));
+        orderBoard.addOrder(new Order(OrderType.BUY, "userid2", "cointype", 11, 600));
+        orderBoard.addOrder(new Order(OrderType.SELL, "userid3", "cointype", 10, 500));
+        orderBoard.addOrder(new Order(OrderType.SELL, "userid4", "cointype", 11, 400));
+
+        HashMap<Integer, Integer> expectedOutput = new LinkedHashMap<>();
+        expectedOutput.put(400, 21);
+        expectedOutput.put(500, 10);
+
+        Assertions.assertThat(orderBoard.viewLiveOrders(OrderType.SELL)).isEqualTo(expectedOutput);
+    }
+
+    @Test
+    void testBuyWhenSamePriceForDifferentOrders() {
+        orderBoard.addOrder(new Order(OrderType.BUY, "userid1", "cointype", 10, 400));
+        orderBoard.addOrder(new Order(OrderType.BUY, "userid2", "cointype", 11, 600));
+        orderBoard.addOrder(new Order(OrderType.BUY, "userid3", "cointype", 13, 600));
+        orderBoard.addOrder(new Order(OrderType.SELL, "userid4", "cointype", 11, 400));
+
+        HashMap<Integer, Integer> expectedOutput = new LinkedHashMap<>();
+        expectedOutput.put(600, 24);
+        expectedOutput.put(400, 10);
+
+        Assertions.assertThat(orderBoard.viewLiveOrders(OrderType.BUY)).isEqualTo(expectedOutput);
+    }
+
+    @Test
+    void testBuyWhenCanceledOneOrder() {
+        orderBoard.addOrder(new Order(OrderType.BUY, "userid1", "cointype", 10, 400));
+        orderBoard.addOrder(new Order(OrderType.BUY, "userid2", "cointype", 11, 600));
+        orderBoard.addOrder(new Order(OrderType.BUY, "userid3", "cointype", 13, 600));
+        orderBoard.addOrder(new Order(OrderType.SELL, "userid4", "cointype", 11, 400));
+        orderBoard.cancelOrder(new Order(OrderType.BUY, "userid3", "cointype", 13, 600));
+
+
+        HashMap<Integer, Integer> expectedOutput = new LinkedHashMap<>();
+        expectedOutput.put(600, 11);
+        expectedOutput.put(400, 10);
+
+        Assertions.assertThat(orderBoard.viewLiveOrders(OrderType.BUY)).isEqualTo(expectedOutput);
+    }
+
+    @Test
+    void testSellWhenCanceledOneOrder() {
+        orderBoard.addOrder(new Order(OrderType.SELL, "userid1", "cointype", 10, 400));
+        orderBoard.addOrder(new Order(OrderType.BUY, "userid2", "cointype", 11, 600));
+        orderBoard.addOrder(new Order(OrderType.SELL, "userid3", "cointype", 13, 600));
+        orderBoard.addOrder(new Order(OrderType.SELL, "userid4", "cointype", 11, 400));
+        orderBoard.cancelOrder(new Order(OrderType.SELL, "userid3", "cointype", 13, 600));
+
+
+        HashMap<Integer, Integer> expectedOutput = new LinkedHashMap<>();
+        expectedOutput.put(400, 21);
+
+        Assertions.assertThat(orderBoard.viewLiveOrders(OrderType.SELL)).isEqualTo(expectedOutput);
+    }
+
+    @Test
+    void testSellWhenCancellingNonExistingOneOrder() {
+        orderBoard.addOrder(new Order(OrderType.SELL, "userid1", "cointype", 10, 400));
+        orderBoard.addOrder(new Order(OrderType.BUY, "userid2", "cointype", 11, 600));
+        orderBoard.addOrder(new Order(OrderType.SELL, "userid3", "cointype", 13, 600));
+        orderBoard.addOrder(new Order(OrderType.SELL, "userid4", "cointype", 11, 400));
+        orderBoard.cancelOrder(new Order(OrderType.SELL, "userid4", "cointype", 13, 600));
+
+
+        HashMap<Integer, Integer> expectedOutput = new LinkedHashMap<>();
+        expectedOutput.put(400, 21);
+        expectedOutput.put(600, 13);
+
+        Assertions.assertThat(orderBoard.viewLiveOrders(OrderType.SELL)).isEqualTo(expectedOutput);
+    }
 }
