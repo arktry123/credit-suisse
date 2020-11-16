fe6702cbcee62e5fc9ae324e2addf5995ed729b2
Ramakrishna <arktry123@gmail.com>
2020-11-16 01:21:00 +0000

    Adding a basic in-memory implementation

--- src/main/java/com/company/crypto/app/InMemoryLiveOrderBoardImpl.java
+++ src/main/java/com/company/crypto/app/InMemoryLiveOrderBoardImpl.java
@@ -11,10 +11,7 @@ import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.summingInt;
 
 public class InMemoryLiveOrderBoardImpl implements LiveOrderBoard {
-    private static final int LIVE_ORDERS_MAX_SIZE = 10;
-    List<Order> orders = new ArrayList<>();
-    Comparator<Map.Entry<Integer, Integer>> sellComparator = Map.Entry.comparingByKey();
-    Comparator<Map.Entry<Integer, Integer>> buyComparator = sellComparator.reversed();
+    private List<Order> orders = new ArrayList<>();
 
     @Override
     public void addOrder(Order order) {
@@ -28,28 +25,7 @@ public class InMemoryLiveOrderBoardImpl implements LiveOrderBoard {
 
     @Override
     public Map<Integer, Integer> viewLiveOrders(OrderType orderType) {
-        Comparator<Map.Entry<Integer, Integer>> comparator = getComparator(orderType);
-
-        Map<Integer, Integer> unsorted = orders.stream().filter(o -> o.getOrderType().equals(orderType))
-                .map(o -> new LiveOrder(o.getPricePerCoin(), o.getQuantity()))
-                .collect(groupingBy(LiveOrder::getPrice, summingInt(LiveOrder::getQuantity)));
-
-        Map<Integer, Integer> sorted = unsorted.entrySet().stream()
-                .sorted(comparator)
-                .limit(LIVE_ORDERS_MAX_SIZE)
-                .collect(Collectors.toMap(e -> e.getKey(), e -> e.getValue(),
-                        (oldValue, newValue) -> oldValue, LinkedHashMap::new));
-        return sorted;
-
+        return null;
     }
 
-    private Comparator<Map.Entry<Integer, Integer>> getComparator(OrderType orderType) {
-        Comparator<Map.Entry<Integer, Integer>> comparator;
-        if (orderType.equals(OrderType.SELL)) {
-            comparator = sellComparator;
-        } else {
-            comparator = buyComparator;
-        }
-        return comparator;
-    }
 }
